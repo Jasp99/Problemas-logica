a3e4d726399dcac5dd5a627ccd86e9976062fe8e
Jasp99 <59714656+Jasp99@users.noreply.github.com>
2020-01-14 21:55:55 -0700

    Add files via upload
    Problemas de logica requeridos para Hackademy

---
+++ number-to-string.js
@@ -0,0 +1,19 @@
+//Crea un archivo llamado number-to-string.js.
+
+//En ese archivo define una variable llamada n que referencie el número 128;
+
+//LLama al método .toString() de esa variable n.
+
+//Utiliza console.log() para imprimir los resultados de .toString() a la terminal.
+
+//Comprueba si tu programa es correcto ejecutando el siguiente comando:
+
+//javascripting verify number-to-string.js
+
+var n = 128;
+
+n.toString()
+
+console.log(n);
+
+
---
+++ strings.js
@@ -0,0 +1,16 @@
+//Para este ejercicio, crea un archivo llamado strings.js.
+
+//En ese archivo define una variable llamada someString de la siguiente forma:
+
+//const someString = 'this is a string'
+//Utiliza console.log para imprimir la variable someString a la terminal.
+
+//Comprueba si tu programa es correcto ejecutando el siguiente comando:
+
+//javascripting verify strings.js
+
+
+const someString = 'this is a string';
+
+console.log(someString);
+
